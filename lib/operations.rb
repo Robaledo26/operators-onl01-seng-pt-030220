79c25b3256d2222f99dc94d43f08cc39b6d7f9b2
celebrated-strategy-2405 <robertaledo22@gmail.com>
2020-03-09 02:03:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,9 +1,18 @@
 ## Inputs --> number (interger)
 ## Outputs --> answer the question am i unsafe -->boolean ()
+#of upir speed is > 60, say TRUE
+# of upir speed is <40, say TRUE
+
+#if you speed is > 60 or your is < 40, say TRUE
+# otherwise, say FALSE
 
 
 def unsafe?(speed)
-
+ if speed > 60 || speed < 40
+   return true 
+else
+  return false
+ end
 end
 
 
